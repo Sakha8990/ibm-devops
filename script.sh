4bd7763776291a330687638f39a802e14dc87fa9
Sakha8990 <64205395+Sakha8990@users.noreply.github.com>
2020-04-29 22:20:06 +0530

    IDT-2

--- script.sh
+++ script.sh
@@ -5,3 +5,4 @@
   echo "New Changes added in Remote UI from newly created branch"
   echo "Difference between local and remote branch"
   echo "Sakthivel did the new changes"
+  echo "JIRA STORY INTEGRATION WITH Github"
